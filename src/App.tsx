9b2f582a5416330c5d972c5c337c7e05a96aa88c
Michel Perez <michel.ingesoft@gmail.com>
2019-10-05 16:55:08 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styles from "./App.module.css"
 
-const App: React.FC = () => {
+const App: React.FC<{ initial?: number }> = ({ initial = 0 }) => {
+  const [clicks, setClicks] = React.useState(initial)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p className={styles.link}>Clicks: {clicks}</p>
+      <button onClick={() => setClicks(clicks + 1)}>+</button>
+      <button onClick={() => setClicks(clicks - 1)}>-</button>
+    </>
+  )
 }
 
-export default App;
+export default App
